7cd401de4593d69c1f24141d1b60e667ffc0875d
Julianna Silva <87389046+jumrsYU@users.noreply.github.com>
2024-02-06 18:55:38 -0500

    Initial implementation of Vehicle class

---
+++ Vehicle.java
@@ -0,0 +1,93 @@
+package main;
+
+/*
+ * Created by: Julianna Silva
+ * Basic initial implementation of the Vehicle class
+ */
+
+public class Vehicle {
+	// Client owner; this will be implemented later for method getOwner
+	private int vehicleID, year;
+	private String type, make, model, colour, licensePlate, registration;
+	
+	public Vehicle() {}
+	
+	public Vehicle(int vehicleID, String type, String make, String model, String colour, String licensePlate, String registration) {
+		this.vehicleID = vehicleID;
+		this.type = type;
+		this.make = make;
+		this.model = model;
+		this.colour = colour;
+		this.licensePlate = licensePlate;
+		this.registration = registration;
+	}
+
+	public void getOwner() {
+		// this method will return the vehicle's owner name and/or ID;
+	}
+	
+	
+	public int getVehicleID() {
+		return vehicleID;
+	}
+
+	public void setVehicleID(int vehicleID) {
+		this.vehicleID = vehicleID;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+
+	public void setLicensePlate(String licensePlate) {
+		this.licensePlate = licensePlate;
+	}
+
+	public String getRegistration() {
+		return registration;
+	}
+
+	public void setRegistration(String registration) {
+		this.registration = registration;
+	}
+}
