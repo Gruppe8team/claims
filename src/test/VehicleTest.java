0bfca2e8cdc2fc0cf78b2b738343d83b909221f6
jumrs <jumrs@10.0.0.225>
2024-02-06 19:40:18 -0500

    Added more tests for the Vehicle class

--- src/test/VehicleTest.java
+++ src/test/VehicleTest.java
@@ -6,6 +6,11 @@ import org.junit.jupiter.api.Test;
 
 import main.Vehicle;
 
+/*
+ * Created by: Julianna Silva
+ * Testing basic implementation of the Vehicle class
+ */
+
 class VehicleTest {
 
 	@Test
@@ -20,6 +25,29 @@ class VehicleTest {
 		assertEquals("Red", car1.getColour());
 		assertEquals("AAA000", car1.getLicensePlate());
 		assertEquals("Premium", car1.getRegistration());
+	}
+	
+	@Test
+	void setVehicleInfoTest() {
+		
+		Vehicle car2 = new Vehicle();
+		
+		car2.setVehicleID(2);
+		car2.setType("Truck");
+		car2.setMake("Tesla");
+		car2.setModel("Cybertruck");
+		car2.setColour("Silver");
+		car2.setLicensePlate("BBB111");
+		car2.setRegistration("Standard");
+
+		assertEquals(2, car2.getVehicleID());
+		assertEquals("Truck", car2.getType());
+		assertEquals("Tesla", car2.getMake());
+		assertEquals("Cybertruck", car2.getModel());
+		assertEquals("Silver", car2.getColour());
+		assertEquals("BBB111", car2.getLicensePlate());
+		assertEquals("Standard", car2.getRegistration());
+		
 	}
 
 }
