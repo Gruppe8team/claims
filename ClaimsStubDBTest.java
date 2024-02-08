82a65a32fd36bdb06e7e71e8b588983b2f3579b0
zuker-joshua <90483754+zuker-joshua@users.noreply.github.com>
2024-02-08 18:59:10 -0500

    Added test case for Stub Database

---
+++ ClaimsStubDBTest.java
@@ -0,0 +1,49 @@
+package claims;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ClaimsStubDBTest {
+	
+	Claims claim1 = new Claims();
+
+	@Test
+	void shouldAddandRemoveClaimstoDB() {
+		claim1.setClaimID(1);
+		claim1.setClientID(100);
+		claim1.setAdvisorID(110);
+		claim1.setPolicyID(2);
+		claim1.setClaimStatus("Closed");
+		claim1.setDateFilled("01/08/24");
+		claim1.setAccidentTime("9:30AM");
+		claim1.setDamage("Windshield cracked, bumper came off");
+		claim1.setTotalled(false);
+		claim1.setAtFault(true);
+		claim1.setPayInfo("$500");
+		claim1.setDescription("Client rear-ended third party, "+"third party has witnesses");
+		claim1.setClosed(true);
+		claim1.setClosureCond("Made final Payment");
+		ClaimsStubDB.addClaim(claim1);
+		assertEquals(
+				"Claim ID: " + claim1.getClaimID() + "\n" +
+				"Client ID: " + claim1.getClientID() + "\n" +
+				"Advisor ID: " + claim1.getAdvisorID() + "\n" +
+				"Policy ID: " + claim1.getPolicyID() + "\n" +
+				"Current Claim Status: " + claim1.getClaimStatus() + "\n" +
+				"Claim Date: " + claim1.getDateFilled() + "\n" +
+				"Time of Accident: " + claim1.getAccidentTime() + "\n" +
+				"Vehicle Damages: " + claim1.getDamage() + "\n" +
+				"Write-off: " + claim1.isTotalled() + "\n" +
+				"Driver at Fault: "+ claim1.isAtFault() + "\n" +
+				"Payment Status: "+ claim1.getPayInfo() + "\n" +
+				"Accident Description: "+ claim1.getDescription() + "\n" +
+				"Claim Closed: "+ claim1.isClosed() + "\n" +
+				"Conditions of Closure: "+ claim1.getClosureCond()
+				, ClaimsStubDB.readClaimInfo());
+			
+		ClaimsStubDB.removeClaim(claim1);
+		assertEquals("", ClaimsStubDB.readClaimInfo());
+	}
+	
+}
