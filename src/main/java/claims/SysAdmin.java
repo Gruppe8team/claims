02dd6b379c172a5316a9856270635c3b06868853
A Programmer <tocyrusyang@gmail.com>
2024-02-17 00:55:48 -0500

    added constructor and basic functionality

--- src/main/java/claims/SysAdmin.java
+++ src/main/java/claims/SysAdmin.java
@@ -1,9 +1,34 @@
 package src.main.java.claims;
 
+import java.util.ArrayList;
+
 /*
-* Sys
+* SysAdmin Class
+* For Management purposes
  */
 public class SysAdmin extends User  {
 
+    private boolean isActive;
+    public SysAdmin() {
+        super(); // Call superclass constructor
+        super.setTypeAccount(3);
+    }
+
+    // Constructor with parameters
+    public SysAdmin(int userID, String passwordKey, String firstName, String lastName, String email, boolean isActive) {
+        super(userID, passwordKey, firstName, lastName, email); // Call superclass constructor
+        super.setTypeAccount(3);
+        this.isActive = isActive;
+    }
+
+    public SysAdmin(SysAdmin sysAdmin){
+        super(sysAdmin);
+        super.getTypeAccount();
+        this.isActive = sysAdmin.getIsActive();
+    }
+
+    private boolean getIsActive() {
+        return isActive;
+    }
 
 }
