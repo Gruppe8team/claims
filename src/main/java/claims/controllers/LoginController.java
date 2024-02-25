0f19563ff3e739337a7ec946a2548f463624a280
Kyle Williamson <mgkshome7@gmail.com>
2024-02-24 23:04:08 -0500

    Made login page

---
+++ src/main/java/claims/controllers/LoginController.java
@@ -0,0 +1,33 @@
+package claims.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class LoginController {
+
+    @FXML
+    private Button Button_SignIn;
+
+    @FXML
+    private Button Button_SignUp;
+
+    @FXML
+    private TextField TextField_Email;
+
+    @FXML
+    private TextField TextField_Password;
+
+    @FXML
+    void Button_SignInClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Button_SignUpClicked(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/claims/javaFX.java
+++ src/main/java/claims/javaFX.java
@@ -1,7 +1,11 @@
 package claims;
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,37 +18,17 @@ public class javaFX extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Login");
-
-        // Create a grid pane
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
-
-        // Add UI components
-        Label userName = new Label("Username:");
-        grid.add(userName, 0, 1);
-
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 1);
-
-        Label pw = new Label("Password:");
-        grid.add(pw, 0, 2);
-
-        PasswordField pwBox = new PasswordField();
-        grid.add(pwBox, 1, 2);
-
-        Button btn = new Button("Sign in");
-        grid.add(btn, 1, 4);
-
-        // Set the scene
-        Scene scene = new Scene(grid, 300, 275);
-        primaryStage.setScene(scene);
-
-        // Show the stage
-        primaryStage.show();
+        
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setTitle("Login");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+        }
+        
     }
 
     public static void main(String[] args) {
