6e2e804c5f9452af556b571d98985f9e13948e9f
Kyle Williamson <mgkshome7@gmail.com>
2024-02-24 23:26:53 -0500

    renamed Login

--- src/main/java/claims/Login.java
+++ src/main/java/claims/Login.java
@@ -7,7 +7,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class javaFX extends Application {
+public class Login extends Application {
 
     @Override
     public void start(Stage primaryStage) {
