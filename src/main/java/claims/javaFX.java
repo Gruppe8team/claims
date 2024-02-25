0bd9c767889c6ff0c17375ba8ce9039a2ec97874
Kyle Williamson <mgkshome7@gmail.com>
2024-02-24 23:05:04 -0500

    editing login

--- src/main/java/claims/javaFX.java
+++ src/main/java/claims/javaFX.java
@@ -3,15 +3,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class javaFX extends Application {
