50016e57c82b26d0ce8816c722c574a173e89316
Kyle Williamson <mgkshome7@gmail.com>
2024-02-29 12:58:38 -0500

    connected the controller to the fxml

--- src/main/java/claims/CustomerMenuController.java
+++ src/main/java/claims/CustomerMenuController.java
@@ -1,6 +1,20 @@
 package claims;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class CustomerMenuController {
+
+    @FXML
+    private Button home_btn;
+    
+    @FXML
+    private Button Insurance_btn;
+
+    @FXML
+    private Button claims_btn;
+
+    @FXML
+    private Button logout_btn;
 
 }
