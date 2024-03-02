96798cdb726c90d5c9c11ae33fa6b749c6b4ebaf
Kyle Williamson <mgkshome7@gmail.com>
2024-03-02 12:56:04 -0500

    Created HomeController

---
+++ src/main/java/claims/HomeController.java
@@ -0,0 +1,38 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+
+public class HomeController {
+
+    @FXML
+    private Label acc_dob;
+
+    @FXML
+    private Label acc_email;
+
+    @FXML
+    private Label acc_gender;
+
+    @FXML
+    private Label acc_name;
+
+    @FXML
+    private Label acc_phonenumber;
+
+    @FXML
+    private Label acc_signupdate;
+
+    @FXML
+    private Label acc_type;
+
+    @FXML
+    private ListView<?> claims_listview;
+
+    @FXML
+    private Label login_date;
+
+    @FXML
+    private Text user_name;
+
+}
