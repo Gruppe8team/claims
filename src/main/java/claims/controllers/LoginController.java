40539454cadc9a364d419af580766e3e14a2e7a3
TejaeW11 <114426336+TejaeW11@users.noreply.github.com>
2024-03-11 15:55:27 -0400

    Add files via upload

--- src/main/java/claims/controllers/LoginController.java
+++ src/main/java/claims/controllers/LoginController.java
@@ -1,8 +1,8 @@
-package claims.controllers;
+package application;
 
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
-import claims.models.Model;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -27,19 +27,56 @@ public class LoginController implements Initializable {
     private TextField TextField_Password;
 
     @FXML
-	private ChoiceBox acc_type_selector;
+	private ChoiceBox<String> acc_type_selector = new ChoiceBox<>();
+    
+    private String[] acc_type = {"Client", "Advisor"};
 
     @Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 		Button_SignIn.setOnAction(event -> onLogin());
         Button_SignUp.setOnAction(event -> onRegister());
+        acc_type_selector.getItems().addAll(acc_type);
 	}
 
 
     private void onLogin() {
-        Stage stage = (Stage) Button_SignIn.getScene().getWindow();
+    	
+//    	String email; 
+//    	String password;
+//    	
+//    	
+//    	if(acc_type_selector.getValue() == acc_type[0]) {
+//    		try {
+//        		customerDAO cusdao = new customerDAO();
+//        		email = cusdao.getEmailInfo();
+//        		
+//        		Stage stage = (Stage) Button_SignIn.getScene().getWindow();
+//    	        Model.getInstance().getViewFactory().closeStage(stage);
+//    	        Model.getInstance().getViewFactory().showCustomerWindow();
+//        	}catch(SQLException e) {
+//        		e.printStackTrace();
+//        	}
+//    	}
+//    	else if (acc_type_selector.getValue() == acc_type[1]) {
+//    		try {
+//        		advisorDAO advdao = new advisorDAO();
+//        		email = advdao.getEmailInfo();
+//        		
+//        		
+//        		
+//        		Stage stage = (Stage) Button_SignIn.getScene().getWindow();
+//                Model.getInstance().getViewFactory().closeStage(stage);
+//                Model.getInstance().getViewFactory().showAdvisorMenu();
+//        		
+//        	}catch(SQLException e) {
+//        		e.printStackTrace();
+//        	}
+//    	}
+    	
+    	Stage stage = (Stage) Button_SignIn.getScene().getWindow();
         Model.getInstance().getViewFactory().closeStage(stage);
         Model.getInstance().getViewFactory().showCustomerWindow();
+    		
     }
 
     public void onRegister() {
