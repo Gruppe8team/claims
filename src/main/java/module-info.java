83f18fa7886b1bd9be47070cb21451f65f425118
Kyle Williamson <mgkshome7@gmail.com>
2024-03-10 23:16:19 -0400

    edit page now shows when the edit button is clicked

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module claims {
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires java.desktop;
-    exports claims;
-    exports claims.controllers;
-    opens claims.controllers to javafx.fxml;
-}
