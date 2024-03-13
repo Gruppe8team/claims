bf30194a09e387b24c6963814efc6680ad67b0ad
Kyle Williamson <mgkshome7@gmail.com>
2024-03-13 12:20:22 -0400

    Admin menu controller

--- src/main/java/claims/controllers/Admin/AdminMenuController.java
+++ src/main/java/claims/controllers/Admin/AdminMenuController.java
@@ -3,22 +3,30 @@ package claims.controllers.Admin;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import claims.models.Model;
+import claims.views.AdminMenuOptions;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 public class AdminMenuController implements Initializable {
     @FXML
-	private Button homeButton;
+	private Button home_btn;
 	
 	@FXML
-	private Button policyButton;
+	private Button customers_btn;
 	
 	@FXML
-	private Button claimsButton;
+	private Button advisors_btn;
+
+    @FXML
+    private Button claims_btn;
+
+    @FXML
+    private Button policy_btn;
 	
 	@FXML
-	private Button logoutButton;
+	private Button logout_btn;
 	
 	@Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -26,9 +34,36 @@ public class AdminMenuController implements Initializable {
     }
 	
 	private void addListeners() {
-        homeButton.setOnAction(event -> onHome());
-        claimsButton.setOnAction(event -> onClaims());
-        policyButton.setOnAction(event -> onPolicy());
+        home_btn.setOnAction(event -> onHome());
+        customers_btn.setOnAction(event -> onCustomers());
+        advisors_btn.setOnAction(event -> onAdvisors());
+        claims_btn.setOnAction(event -> onClaims());
+        policy_btn.setOnAction(event -> onPolicy());
+        logout_btn.setOnAction(event -> onLogout());
+    }
+
+    private void onHome() {
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().set(AdminMenuOptions.HOME);
+    }
+
+    private void onCustomers() {
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().set(AdminMenuOptions.CUSTOMERS);
+    }
+
+    private void onAdvisors() {
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().set(AdminMenuOptions.ADVISORS);
+    }
+
+    private void onClaims() {
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().set(AdminMenuOptions.CLAIMS);
+    }
+
+    private void onPolicy() {
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().set(AdminMenuOptions.POLICY);
+    }
+
+    private void onLogout() {
+        Model.getInstance().getViewFactory().getAdminSelectedMenuItem().set(AdminMenuOptions.LOGOUT);
     }
     
 }
