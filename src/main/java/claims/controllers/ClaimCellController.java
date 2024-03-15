d74917089b865b0c85c41091fb0af67f653c1c1a
Kyle Williamson <mgkshome7@gmail.com>
2024-03-15 18:02:39 -0400

    ClaimCellController

---
+++ src/main/java/claims/controllers/ClaimCellController.java
@@ -0,0 +1,28 @@
+package claims.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ClaimCellController {
+    @FXML
+    private Label accident_lbl;
+
+    @FXML
+    private Label claimid_lbl;
+
+    @FXML
+    private Label claimstat_lbl;
+
+    @FXML
+    private Label clientid_lbl;
+
+    @FXML
+    private Label damage_lbl;
+
+    @FXML
+    private Label datefiled_lbl;
+
+    @FXML
+    private Label name_lbl;
+
+}
