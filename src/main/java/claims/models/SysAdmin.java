6b54e4f4bdb139a1444129055d592fb30fe3fe63
Kyle Williamson <mgkshome7@gmail.com>
2024-03-16 13:40:10 -0400

    changes to classes

--- src/main/java/claims/models/SysAdmin.java
+++ src/main/java/claims/models/SysAdmin.java
@@ -8,21 +8,15 @@ package claims.models;
 public class SysAdmin extends User  {
 
     private boolean isActive;
-    public SysAdmin() {
-        super(); // Call superclass constructor
-        super.setTypeAccount(3);
-    }
 
     // Constructor with parameters
     public SysAdmin(int userID, String passwordKey, String firstName, String lastName, String email, boolean isActive) {
         super(userID, passwordKey, firstName, lastName, email); // Call superclass constructor
-        super.setTypeAccount(3);
         this.isActive = isActive;
     }
 
     public SysAdmin(SysAdmin sysAdmin){
         super(sysAdmin);
-        super.getTypeAccount();
         this.isActive = sysAdmin.getIsActive();
     }
 
