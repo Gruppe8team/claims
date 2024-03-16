dcbb7fa92eeec7f10767f9289f0eacdff4d973d0
Kyle Williamson <mgkshome7@gmail.com>
2024-03-16 18:17:49 -0400

    Fixed Advisor Class

--- src/main/java/claims/models/Advisor.java
+++ src/main/java/claims/models/Advisor.java
@@ -3,21 +3,36 @@ package claims.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 public class Advisor extends User {
-	private List<Customer> clients; // Assuming there is a Client class that extends User
-        private List<Claims> managedClaims;
+	private ListProperty<Customer> clients; // Assuming there is a Client class that extends User
+        private ListProperty<Claims> claims;
 	//to initailize an advisor object
+
+        public Advisor() {
+                super();
+                ObservableList<Customer> customerObservableList = FXCollections.observableArrayList();
+                this.clients = new SimpleListProperty<Customer>(customerObservableList);
+                ObservableList<Claims> claimsObservableList = FXCollections.observableArrayList();
+                this.claims = new SimpleListProperty<Claims>(claimsObservableList);
+                
+        }
+
 	public Advisor(int userID, String username, String passwordKey, String firstName, String lastName, String email) {
-        super(userID, username, passwordKey, firstName, lastName, email); // Call superclass constructor
-        this.clients = new ArrayList<>();
-        this.managedClaims = new ArrayList<>();
+                super(userID, username, passwordKey, firstName, lastName, email); // Call superclass constructor
+                this.clients = new SimpleListProperty<Customer>();
+                this.claims = new SimpleListProperty<Claims>();
 	}
 
-    public Advisor(Advisor advisor) {
+        public Advisor(Advisor advisor) {
                 super(advisor); // Call superclass constructor
-                this.clients = new ArrayList<Customer>();
-                this.managedClaims = new ArrayList<Claims>();
-    }
+                this.clients = advisor.getClients();
+                this.claims = advisor.getClaims();
+        }
 	//add a client to the client list
         public void addClient(Customer client) {
                 this.clients.add(client);
@@ -28,21 +43,21 @@ public class Advisor extends User {
         }
         //manage a claim by adding it to the list of managed claims
         public void manageClaim(Claims claim) {
-                this.managedClaims.add(claim);
+                this.claims.add(claim);
         }
         //unmanage a claim by adding it to the list of managed claims
         public void unmanageClaim(Claims claim) {
-                this.managedClaims.remove(claim);
+                this.claims.remove(claim);
         }
         
             // Getters and setters for new fields
         
-        public List<Customer> getClients() {
+        public ListProperty<Customer> getClients() {
                 return clients;
         }
         
-        public List<Claims> getManagedClaims() {
-                return managedClaims;
+        public ListProperty<Claims> getClaims() {
+                return claims;
         }
         
 	 // Override toString method to include customer-specific information
--- src/main/java/claims/models/Model.java
+++ src/main/java/claims/models/Model.java
@@ -14,7 +14,10 @@ public class Model {
     private AccountType loginAccountType = AccountType.CUSTOMER;
     // Customer Data Section
     private final Customer customer;
+    private final Advisor advisor;
     private boolean customerLoginSuccessFlag;
+    private boolean AdvisorLoginSuccessFlag;
+    private boolean AdminLoginSuccessFlag;
     // Adivsor Data Section
 
     // Admin Data Section
@@ -26,7 +29,8 @@ public class Model {
         this.customerLoginSuccessFlag = false;
         this.customer = new Customer(); // Fixed constructor error
         // Adivsor Data Section
-
+        this.AdvisorLoginSuccessFlag = false;
+        this.advisor = new Advisor();
         // Admin Data Section
     }
 
@@ -74,6 +78,12 @@ public class Model {
                 this.customer.getFirstName().set(resultSet.getString("FirstName"));
                 this.customer.getLastName().set(resultSet.getString("LastName"));
                 this.customer.getUsername().set(resultSet.getString("Username"));
+                this.customer.getAddress().set(resultSet.getString("Address"));
+                this.customer.getEmail().set(resultSet.getString("Email"));
+                this.customer.getPhoneNumber().set(resultSet.getString("PhoneNumber"));
+                this.customer.getUserID().set(resultSet.getInt("ID"));
+                this.customer.getgender().set(resultSet.getString("Gender"));
+                this.customer.getAge().set(resultSet.getInt("Age"));
                 this.customerLoginSuccessFlag = true;
             }
         } catch (SQLException e) {
