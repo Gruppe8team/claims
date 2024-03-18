330f9f6b01f9735402b415e26cc3ce3be4b58249
Kyle Williamson <mgkshome7@gmail.com>
2024-03-18 16:56:47 -0400

    update

---
+++ src/main/java/claims/models/Drivers/ClaimDatabaseDriver.java
@@ -0,0 +1,17 @@
+package claims.models.Drivers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ClaimDatabaseDriver {
+    private Connection conn;
+
+    public ClaimDatabaseDriver() {
+        try {
+            this.conn = DriverManager.getConnection("jdbc:sqlite:claims.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
