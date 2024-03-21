eebe034c87adb3e3a3fae7772433d9307b85b45d
Kyle Williamson <mgkshome7@gmail.com>
2024-03-21 17:19:10 -0400

    Update

--- src/main/java/claims/models/Model.java
+++ src/main/java/claims/models/Model.java
@@ -89,11 +89,11 @@ public class Model {
                 this.customer.getUsername().set(resultSet.getString("Username"));
                 this.customer.getAddress().set(resultSet.getString("Address"));
                 this.customer.getEmail().set(resultSet.getString("Email"));
-                this.customer.getPhoneNumber().set(resultSet.getString("PhoneNumber"));
-                this.customer.getUserID().set(resultSet.getInt("ID"));
-                this.customer.getGender().set(resultSet.getString("Gender"));
+                this.customer.getPhoneNumber().set(resultSet.getString("Phone"));
+                this.customer.getUserID().set(resultSet.getInt("ClientID"));
+                this.customer.getGender().set(resultSet.getString("Sex"));
                 this.customer.getAge().set(resultSet.getInt("Age"));
-                this.customer.getPasswordKey().set(resultSet.getString("Password"));
+                this.customer.getPasswordKey().set(resultSet.getString("Password"));                
                 this.customerLoginSuccessFlag = true;
             }
         } catch (SQLException e) {
