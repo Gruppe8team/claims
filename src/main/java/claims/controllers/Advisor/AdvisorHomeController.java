be30339f6c80212175cf2792ed9b83d14d739c49
Kyle Williamson <mgkshome7@gmail.com>
2024-03-22 08:21:19 -0400

    Advisor home

--- src/main/java/claims/controllers/Advisor/AdvisorHomeController.java
+++ src/main/java/claims/controllers/Advisor/AdvisorHomeController.java
@@ -1,5 +1,52 @@
 package claims.controllers.Advisor;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+
 public class AdvisorHomeController {
     
+    @FXML
+    private Label acc_dob;
+
+    @FXML
+    private Label acc_email;
+
+    @FXML
+    private Label acc_gender;
+
+    @FXML
+    private Label acc_name;
+
+    @FXML
+    private Label acc_phonenumber;
+
+    @FXML
+    private Label acc_signupdate;
+
+    @FXML
+    private Label acc_type;
+
+    @FXML
+    private Button btn_edit;
+
+    @FXML
+    private ListView<?> clm_listview;
+
+    @FXML
+    private TableColumn<?, ?> email_col;
+
+    @FXML
+    private Label hi_lbl;
+
+    @FXML
+    private TableColumn<?, ?> id_col;
+
+    @FXML
+    private TableColumn<?, ?> name_col;
+
+    @FXML
+    private Label today_lbl;
 }
