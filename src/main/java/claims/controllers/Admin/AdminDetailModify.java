32e8a49006698b3dd93577f38898b71ea58c1357
A Programmer <tocyrusyang@gmail.com>
2024-03-27 13:20:16 -0400

    New adminstrative portion

---
+++ src/main/java/claims/controllers/Admin/AdminDetailModify.java
@@ -0,0 +1,24 @@
+package claims.controllers.Admin;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import claims.controllers.Customer.EditController;
+import claims.models.Model;
+import claims.models.NewUser;
+import claims.views.AccountType;
+import databases.UserDatabase;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class AdminDetailModify implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+}
