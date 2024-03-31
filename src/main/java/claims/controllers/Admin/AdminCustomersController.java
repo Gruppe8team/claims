e470061a29744156e9985458d56b56b728080e00
A Programmer <tocyrusyang@gmail.com>
2024-03-31 12:25:41 -0400

    admin work

--- src/main/java/claims/controllers/Admin/AdminCustomersController.java
+++ src/main/java/claims/controllers/Admin/AdminCustomersController.java
@@ -1,5 +1,14 @@
 package claims.controllers.Admin;
 
-public class AdminCustomersController {
-    
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminCustomersController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
