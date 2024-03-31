25dfebd18e379aeafcb4b456641cd93fc030c23d
A Programmer <tocyrusyang@gmail.com>
2024-03-31 12:21:52 -0400

    admin work

--- src/main/java/claims/controllers/Admin/AdminClaimsController.java
+++ src/main/java/claims/controllers/Admin/AdminClaimsController.java
@@ -17,7 +17,7 @@ public class AdminClaimsController implements Initializable {
         changelog.setEditable(false);
         loadChangeLogFile();
     }
-
+//
     private void loadChangeLogFile() {
         String filePath = "src/main/resources/claims/changelog.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
