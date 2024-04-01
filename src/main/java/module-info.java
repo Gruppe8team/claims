1e816e397dc0c27382b25618a501cd150ae23d8c
Kyle Williamson <mgkshome7@gmail.com>
2024-04-01 13:24:08 -0400

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,25 +1,5 @@
 
 module claims {
-<<<<<<< HEAD
-        requires javafx.graphics;
-        requires javafx.fxml;
-        requires javafx.controls;
-        requires java.sql;
-    
-        exports claims.controllers;
-        exports claims;
-    
-        opens claims.controllers to
-                javafx.fxml;
-        opens claims.controllers.Customer to
-                javafx.fxml;
-        opens claims.controllers.Advisor to
-                javafx.fxml;
-        opens claims.controllers.Admin to
-                javafx.fxml;
-        opens claims to
-                javafx.graphics;
-=======
     requires javafx.graphics;
     requires javafx.fxml;
     requires javafx.controls;
@@ -39,5 +19,4 @@ module claims {
     opens claims to
             javafx.graphics;
     opens claims.models to javafx.base;
->>>>>>> 8921004aa79f08a393686344216dd94034e0ce20
 }
