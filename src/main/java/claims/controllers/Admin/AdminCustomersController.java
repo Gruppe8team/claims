e27244cdc9c75a968b59988cab1771da7e3f1a98
A Programmer <tocyrusyang@gmail.com>
2024-03-31 20:10:54 -0400

    admin work

--- src/main/java/claims/controllers/Admin/AdminCustomersController.java
+++ src/main/java/claims/controllers/Admin/AdminCustomersController.java
@@ -1,14 +1,59 @@
 package claims.controllers.Admin;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AdminCustomersController implements Initializable {
 
+    @FXML
+    private TableView<User> clm_table;
+
+    @FXML
+    private TableColumn<User, String> userID_col;
+
+    @FXML
+    private TableColumn<User, String> username_col;
+
+    @FXML
+    private TableColumn<User, String> password_col;
+
+    @FXML
+    private TableColumn<User, String> email_col;
+
+    @FXML
+    private TableColumn<User, String> phone_col;
+
+    @FXML
+    private TableColumn<User, String> dob_col;
+
+    @FXML
+    private TableColumn<User, String> status_col;
+
+    @FXML
+    private Text titleText;
+
+    @FXML
+    private Text descriptionText;
+
+    @FXML
+    private TextField confirmationTextField;
+
+    @FXML
+    private Button executeButton;
+
+    @FXML
+    private Button abortButton;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
 }
